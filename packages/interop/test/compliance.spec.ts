f24631673fff9bcf464af03644c04903415a0a2e
achingbrain <alex@achingbrain.net>
2024-10-09 07:12:48 +0100

    chore: linting

--- packages/interop/test/compliance.spec.ts
+++ packages/interop/test/compliance.spec.ts
@@ -1,10 +1,10 @@
 import { createPinningServiceAPIServer } from '@helia/pinning-service-api-server'
+import { expect } from 'aegir/chai'
 import { execa } from 'execa'
 import { createHelia } from 'helia'
 import { nanoid } from 'nanoid'
 import type { FastifyInstance } from 'fastify'
 import type { HeliaLibp2p } from 'helia'
-import { expect } from 'aegir/chai'
 
 describe('pinning service compliance', function () {
   this.timeout(540 * 1000)
